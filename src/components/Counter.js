84f32963899769a1bca0258c82439c1673d092a4
shazaAly <shaza.aly@gmail.com>
2022-11-10 16:42:11 +0200

    dispatch multipe state data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,17 +4,19 @@ import { useSelector, useDispatch } from 'react-redux'
 const Counter = () => {
 
   let count = useSelector(state => state.count)
+  let toggle = useSelector(state=> state.showCounter)
+
   const dispatch = useDispatch()
   const incrementHandler = () => dispatch({ type: 'INCREMENT' })
   const decrementHandler = () => dispatch({ type: 'DECREMENT' })
   const IncAmountHandler = (amount) => dispatch({ type: 'INCREASEAMOUNT', amount: 5 })
-
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler=()=>dispatch({type:'TOGGLE'})
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{count}</div>
+      { toggle && <div className={classes.value}>{count}</div>}
+      
       <button onClick={incrementHandler}> Increment </button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
       <button onClick={decrementHandler}> Decrement </button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,36 @@
 import { createStore } from "redux"
-const counterReducer = (state = { count: 0 }, action) => {
+let initialState = {
+      count: 0 ,
+      showCounter : true
+}
+const counterReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case 'INCREMENT':
             return {
-                count: state.count + 1
+                count: state.count + 1,
+                showCounter: state.showCounter
             }
 
         case 'DECREMENT':
             return {
-                count: state.count - 1
+                count: state.count - 1,
+                showCounter: state.showCounter
+
             }
 
         case 'INCREASEAMOUNT':
             return {
-                count: state.count + action.amount
+                count: state.count + action.amount,
+                showCounter: state.showCounter
+
             }
+            case 'TOGGLE':
+                return {
+                    count: state.count,
+                    showCounter: !state.showCounter
+    
+                }
 
         default:
             return state;
