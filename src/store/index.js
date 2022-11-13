75de0451362581fbb2f72cf99488d3803e486999
shazaAly <shaza.aly@gmail.com>
2022-11-13 10:33:14 +0200

    store and slice ready

--- src/store/index.js
+++ src/store/index.js
@@ -28,44 +28,10 @@ const counterSlice = createSlice({
 
 })
 
-// const counterReducer = (state = initialState, action) => {
-
-//     switch (action.type) {
-//         case 'INCREMENT':
-//             return {
-//                 count: state.count + 1,
-//                 showCounter: state.showCounter
-//             }
-
-//         case 'DECREMENT':
-//             return {
-//                 count: state.count - 1,
-//                 showCounter: state.showCounter
-
-//             }
-
-//         case 'INCREASEAMOUNT':
-//             return {
-//                 count: state.count + action.amount,
-//                 showCounter: state.showCounter
-
-//             }
-//         case 'TOGGLE':
-//             return {
-//                 count: state.count,
-//                 showCounter: !state.showCounter
-
-//             }
-
-//         default:
-//             return state;
-//     }
-
-// }
-// const store = createStore(counterReducer)
 const store= configureStore({
     reducer: counterSlice.reducer
 })
+export const counterActions = counterSlice.actions
 
 export default store
 
--- test.js
+++ test.js
@@ -1,18 +1,27 @@
- const redux = require('redux');
+let myWords = "Hey there, am a fullstack dev in the making, what is your story though"
 
- const counterReducer = (state={count : 0}, action)=>{
-    return {
-        count : state.count + 1
-    }
+function myFun(){
+ 
+    let newWords1 = myWords.split(" ").join("")
+    let newWords2 = newWords1.split("")
+    let newWords3 = newWords2.length
 
- }
+    let y = myWords.split(" ")
+    let x = y.length
+ 
+    let vowels = "aeiouAEIOU"
+    let vowelsA = vowels.split(" ").join("")
+    let vowelsB = vowelsA.split("")
+    let count = 0
 
- const store = redux.createStore(counterReducer)
+    for (let i = 0; i < newWords3; i++) {
+        
+        if (vowelsB.indexOf(newWords2[i]) !== -1) {
+                count += 1;
+            }
+        }
+    let finalResult = console.log(`There are ${x} Words, ${newWords3} letters and ${count} vowels`)
+    return finalResult;
+}
 
- const subscriber = ()=>{
-    const latestState = store.getState()
- }
-
- store.subscribe(subscriber)
-
-
+myFun()
